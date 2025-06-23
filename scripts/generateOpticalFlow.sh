efd65186853174f2c21c5712bce0adca0dd18bec
arao761 <ankrao26@gmail.com>
2025-06-23 14:46:40 -0400

    fixing:

--- scripts/generateOpticalFlow.sh
+++ scripts/generateOpticalFlow.sh
@@ -14,13 +14,13 @@ if [ -f "$flow_raw_file" ]; then
     echo "Optical flow already computed at: $flow_raw_file"
 else
     echo "Submitting job to compute optical flow..."
-    bsub -n 8 -W 01:00 python3 opticalFlow.py "${zarr_folder}"
+    bsub -n 8 -W 01:00 -K python3 opticalFlow.py "${zarr_folder}"
 fi
 
-# submit movie creation job if movie doesn't exist
-#if [ -f "$movie_file" ]; then
- #   echo "Optical flow movie already exists at: $movie_file"
-#else
- #   echo "Submitting job to create optical flow movie..."
-  #  bsub -n 8 -W 01:00 python3 ./makeOpticalFlowMovie.py "${zarr_folder}"
-#fi
+#submit movie creation job if movie doesn't exist
+if [ -f "$movie_file" ]; then
+   echo "Optical flow movie already exists at: $movie_file"
+else
+   echo "Submitting job to create optical flow movie..."
+   bsub -n 8 -W 01:00 python3 ./makeOpticalFlowMovie.py "${zarr_folder}"
+fi
