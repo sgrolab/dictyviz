Fixed bugs when generating average flow from a subset of frames

scripts/OpticalFlow/averageFrames.py
@@ -16,25 +16,30 @@ def main():
         sys.exit(1)
 
     frame_dirs = [d for d in os.listdir(results_dir) if os.path.isdir(os.path.join(results_dir, d))]
+    # remove non-numeric directories
+    frame_dirs = [d for d in frame_dirs if d.isdigit()]
+    frame_dirs.sort(key=int)  # Sort numerically by frame number
 
     # generate log file
     log_file = os.path.join(results_dir, "average_frames.log")
     with open(log_file, 'w') as log:
         try:
+            first_frame = int(frame_dirs[0])
             # calculate the average flow for the first nb_frames
-            flow_data = flowLoader.load_first_frames(results_dir, nb_frames, log_file=log)
+            flow_data = flowLoader.load_first_frames(results_dir, first_frame, nb_frames, log_file=log)
 
             avg_flow = np.mean(flow_data, axis=1)  # average flow across slices
 
-            mid_frame_index = nb_frames // 2
+            mid_frame_index = first_frame + (nb_frames // 2)
             output_dir = os.path.join(results_dir, str(mid_frame_index))
             if not os.path.exists(output_dir):
                 raise FileNotFoundError(f"Output directory {output_dir} does not exist.")
 
-            np.save(os.path.join(output_dir, f"avg_flow_frame_{nb_frames//2}.npy"), avg_flow)
+            np.save(os.path.join(output_dir, f"avg_flow_frame_{mid_frame_index}.npy"), avg_flow)
 
-            for first_frame_index in range(len(frame_dirs)-nb_frames):
-                flow_data = flowLoader.load_next_frame(flow_data, results_dir, first_frame_index + nb_frames, log_file=log)
+            for frame_index in range(len(frame_dirs)-nb_frames):
+                frame_nb = int(frame_dirs[frame_index])
+                flow_data = flowLoader.load_next_frame(flow_data, results_dir, frame_nb + nb_frames, log_file=log)
                 avg_flow = np.mean(flow_data, axis=1)  # average flow across slices
 
                 # save the average flow for this frame

scripts/OpticalFlow/helpers/flowLoader.py
@@ -124,16 +124,16 @@ def extract_slice(flow_data, raw_data=None, idx=None):
     
     return vx, vy, vz, conf, raw_slice, idx
 
-def load_first_frames(results_dir, nb_frames, log_file=None):
+def load_first_frames(results_dir, first_frame, nb_frames, log_file=None):
     """
     Load the first nb_frames of flow data from the results directory.
     """
 
-    flow_frame_0 = load_flow_frame(results_dir, 0)
-    lenZ, lenY, lenX = flow_frame_0['vx'].shape
+    first_flow_frame = load_flow_frame(results_dir, first_frame)
+    lenZ, lenY, lenX = first_flow_frame['vx'].shape
     flow_data = np.zeros((3, nb_frames, lenZ, lenY, lenX), dtype=np.float32)  # initialize flow data array
 
-    for frame in range(nb_frames):
+    for i, frame in enumerate(range(first_frame, first_frame + nb_frames)):
         if log_file:
             log_file.write(f"Loading flow data for frame {frame}...\n")
         else:
@@ -144,10 +144,10 @@ def load_first_frames(results_dir, nb_frames, log_file=None):
         flow_frame_vy = flow_frame['vy']
         flow_frame_vz = flow_frame['vz']
 
-        flow_data[0, frame] = flow_frame_vx
-        flow_data[1, frame] = flow_frame_vy
-        flow_data[2, frame] = flow_frame_vz
-    
+        flow_data[0, i] = flow_frame_vx
+        flow_data[1, i] = flow_frame_vy
+        flow_data[2, i] = flow_frame_vz
+
     return flow_data
 
 def load_next_frame(flow_data, results_dir, frame_index, log_file=None):
@@ -155,9 +155,10 @@ def load_next_frame(flow_data, results_dir, frame_index, log_file=None):
     Load the next frame of flow data and update the flow_data array.
     """
     frame_dirs = [d for d in os.listdir(results_dir) if os.path.isdir(os.path.join(results_dir, d))]
+    frame_range = [int(d) for d in frame_dirs if d.isdigit()]
     
-    if frame_index < 0 or frame_index >= len(frame_dirs):
-        print(f"Error: frame_index {frame_index} is out of bounds for flow_data with shape {flow_data.shape}.")
+    if frame_index not in frame_range:
+        print(f"Error: frame_index {frame_index} is out of bounds for frame directories with range {frame_range[0]} - {frame_range[-1]}.")
         return
 
     if log_file: